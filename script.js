f3aade55fadedb9fa161d7f456d4307662d4887d
ahmedcauni <ahmedcauni@gmail.com>
2025-08-22 02:50:58 -0400

    added functionality to the submit button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const myLib=[];
 function book(title, desc, status=false){
     this.title=title;
     this.desc=desc;
-
+    this.status=status;
 }
 
 function addBookToLib(title, desc)
@@ -14,3 +14,17 @@ function addBookToLib(title, desc)
     myLib.push(newBook);
 
 }
+
+const form=document.getElementById("books");
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const desc= document.getElementById("description").value;
+
+    addBookToLib(title,desc);
+
+    console.log(myLib);
+});
